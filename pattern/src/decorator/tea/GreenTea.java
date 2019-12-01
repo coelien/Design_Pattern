cf05711d478a625b75ad848938b66615bda65f9d
coelien coco <daoxiang@mail.dlut.edu.cn>
2019-12-01 12:55:49 +0800

    add green tea to the tea component

---
+++ pattern/src/decorator/tea/GreenTea.java
@@ -0,0 +1,17 @@
+package decorator.tea;
+
+public class GreenTea implements Tea {
+
+	@Override
+	public String getTeaName() {
+		// TODO Auto-generated method stub
+		return "green tea";
+	}
+
+	@Override
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return 8;
+	}
+
+}
